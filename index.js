4486eee128739e7f21c2ee4d6688fa02343399c6
Sara Jordan <sara.jordan@gmail.com>
2018-01-10 02:07:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ function shout(string) {
 }
 function whisper(string) {
   return string.toLowerCase()
+<<<<<<< HEAD
 }
 
 function sayHiToGrandma(string) {
   return "I can't hear you!"
+=======
+>>>>>>> 2bde4cfe52553e370ae29352a714733e638d53a5
 }
